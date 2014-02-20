8668df989ac67a0127a888a00e8f26844babf8d4
Geet Thaker <gthaker3@gmail.com>
2014-02-20 16:13:34 -0500

    logic added

--- src/practiceroom/tournament/sub1_25/sub2/pro500/Tothello.java
+++ src/practiceroom/tournament/sub1_25/sub2/pro500/Tothello.java
@@ -7,8 +7,39 @@ public class Tothello {
 
 	public int bestMove(String[] redPieces, String[] blackPeices, String whoseTurn) {
 		grid = createGrid(redPieces, blackPeices);
-
+		if (whoseTurn.equals("Red")) {
+			whoseTurn = "R";
+		} else {
+			whoseTurn = "B";
+		}
+		solve(whoseTurn);
 		return 0;
+	}
+
+	private void solve(String whoseTurn) {
+		String find = whoseTurn.equals("R") ? "B" : "R";
+		for (int i = 0; i < emptypeices.length; i++) {
+			int a = Character.getNumericValue(emptypeices[i].charAt(0));
+			int b = Character.getNumericValue(emptypeices[i].charAt(1));
+			int x = a, y = b;
+
+			if (++y > -1 && y < 8) {
+				int count = 0;
+				String con = grid[x][y];
+				if (grid[x][y].equals(find)) {
+					while (y < 8 && grid[x][y].equals(find)) {
+						y++;
+						count++;
+					}
+					if (grid[x][y].equals(whoseTurn)) {
+						for (int j = a + 1; j < y; j++) {
+							grid[x][j] = whoseTurn;
+						}
+					}
+				}
+			}
+			printGrig(grid);
+		}
 	}
 
 	public String[][] createGrid(String[] redPieces, String[] blackPeices) {
@@ -18,27 +49,40 @@ public class Tothello {
 			String v = redPieces[r];
 			System.out.println(v.charAt(0) - 'A');
 			System.out.println(Character.getNumericValue(v.charAt(1)) - 1);
-			g[(v.charAt(0) - 'A')][Character.getNumericValue(v.charAt(1)) - 1] = "R";
+			g[Character.getNumericValue(v.charAt(1)) - 1][(v.charAt(0) - 'A')] = "R";
 		}
 		for (int b = 0; b < blackPeices.length; b++) {
 			String v = blackPeices[b];
-			g[(v.charAt(0) - 'A')][Character.getNumericValue(v.charAt(1)) - 1] = "B";
+			g[Character.getNumericValue(v.charAt(1)) - 1][(v.charAt(0) - 'A')] = "B";
 		}
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				if (g[i][j] == null) {
+					g[i][j] = "-";
 					System.out.println(i + "" + j + "   " + c);
 					emptypeices[c++] = i + "" + j;
 				}
 			}
 		}
+		printGrig(g);
 		return g;
+	}
+
+	public void printGrig(String[][] g) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				System.out.print(g[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println("=====================");
 	}
 
 	public static void main(String[] args) {
 		Tothello t = new Tothello();
-		String[] redPieces = { "A1" };
-		String[] blackPieces = { "C1" };
-		t.bestMove(redPieces, blackPieces, "R");
+		String[] redPieces = { "D1" };
+		String[] blackPieces = { "B1", "C1" };
+		t.bestMove(redPieces, blackPieces, "Red");
 	}
 }
